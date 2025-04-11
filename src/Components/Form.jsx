b4472eae3789592ba642c14ff9f0029c6e4b5922
theMungai <mungaithesimon@gmail.com>
2025-04-11 22:24:01 +0300

    Form structure

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -3,7 +3,34 @@ import React from 'react'
 function Form(){
   return (
     <div className="expense-form">
-        
+        <form >
+            <input 
+                type="text" 
+                placeholder='Expense name' 
+                required 
+            />
+
+            <textarea placeholder='Expense description' required></textarea>
+
+            <input 
+                type="text" 
+                placeholder='Expense category'
+                required
+            />
+
+            <input 
+                type="number"
+                placeholder='Expense amount'
+                required 
+            />
+
+            <input 
+                type="date"
+                required
+            />
+
+            <button type='submit'>Submit</button>
+        </form>
     </div>
   )
 }
