26194fce793be1a3c39b48bb076d1bf06e24e9af
theMungai <mungaithesimon@gmail.com>
2025-04-11 21:51:27 +0300

    Starter files

---
+++ src/Components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='Header'>
+      <h1>Expense Tracker</h1>
+      <p>Start taking control of your finances. Track spending, build habits, and grow your savings—one smart move at a time.</p>
+    </div>
+  )
+}
+
+export default Header
