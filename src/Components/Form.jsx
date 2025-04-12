1028948963866925c6fac84b806d1c9724a9c811
theMungai <mungaithesimon@gmail.com>
2025-04-12 10:51:25 +0300

    controlled inputs functionality

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,36 +1,76 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Form(){
+  const [formData, setFormData] = useState({
+    expenseName: "",
+    description: "",
+    category: "",
+    amount: "",
+    date: ""
+  })
+
+  function handleInputs(event){
+    const {name, value} = event.target
+    setFormData(prev => ({
+        ...prev,
+        [name]: value
+    }))
+  }
+
   return (
     <div className="expense-form">
         <form >
             <input 
                 type="text" 
-                placeholder='Expense name' 
-                required 
+                placeholder='Expense name'
+                name='expenseName' 
+                required
+                value={formData.expenseName}
+                onChange={handleInputs} 
             />
 
-            <textarea placeholder='Expense description' required></textarea>
+            <textarea 
+                placeholder='Expense description' 
+                required
+                name='description'
+                value={formData.description}
+                onChange={handleInputs} 
+            ></textarea>
 
             <input 
                 type="text" 
                 placeholder='Expense category'
                 required
+                name='category'
+                value={formData.category}
+                onChange={handleInputs} 
             />
 
             <input 
                 type="number"
                 placeholder='Expense amount'
-                required 
+                required
+                name='amount'
+                value={formData.amount}
+                onChange={handleInputs}  
             />
 
             <input 
                 type="date"
                 required
+                name='date'
+                value={formData.date}
+                onChange={handleInputs} 
             />
 
             <button type='submit'>Submit</button>
+            <p>{formData.expenseName}</p>
+            <p>{formData.description}</p>
+            <p>{formData.category}</p>
+            <p>{formData.amount}</p>
+            <p>{formData.date}</p>
         </form>
+        
     </div>
   )
 }
